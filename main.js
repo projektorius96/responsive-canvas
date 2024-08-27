37be9510464b1eb8043c121f362ec0bc99f02926
LG <34185919+projektorius96@users.noreply.github.com>
2024-08-27 18:53:21 +0300

    init commit, working draft of the implementation

--- main.js
+++ main.js
@@ -52,6 +52,38 @@ function drawSquare() {
     ctx.fillStyle = 'green';
     ctx.scale(devicePixelRatio, devicePixelRatio)
     ctx.fillRect(x, y, absoluteWidth * size, absoluteHeight * size);
+
+    canvas.addEventListener("mousemove", (event) => {
+    
+      const scale = {
+        x: canvas.width / canvas.getBoundingClientRect().width,
+        y: canvas.height / canvas.getBoundingClientRect().height,
+      }
+        
+      // Create a Path2D object for the rectangle
+      const rectanglePath = new Path2D();
+            rectanglePath.rect(x, y, absoluteWidth * size, absoluteHeight * size);
+  
+      const mouseX = (event.clientX - canvas.getBoundingClientRect().left) * scale.x;  /* <= essentially multiplying by `scale.x` still allows us to point in the centre, but push pointer further **right**, which is present due to Device Pixel Ratio  */ 
+      const mouseY = (event.clientY - canvas.getBoundingClientRect().top) * scale.y;   /* <= essentially multiplying by `scale.y` still allows us to point in the centre, but push pointer further **bottom**, which is present due to Device Pixel Ratio */    
+      
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      
+      // Check if the mouse click is within the rectangle path
+      if ( ctx.isPointInPath(rectanglePath, mouseX, mouseY) ) {
+    
+        ctx.fillStyle = "red";
+    
+      } else {
+    
+        ctx.fillStyle = "green";
+    
+      }
+    
+      ctx.fillRect(x, y, absoluteWidth * size, absoluteHeight * size);
+    
+    });
+
 }
 
 resizeCanvas(); // Initial call to set up the canvas size and draw the square
